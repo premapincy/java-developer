0493c50e2e411ac83b637e8d2b815e51a57f66bd
premapincy <50817404+premapincy@users.noreply.github.com>
2019-05-30 22:32:52 -0700

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,14 @@
+class Digits
+{
+public static void main(String args[])
+{
+int i=1, n1=9, j=2, n2=8;
+for(i=1;i<=n1;i+=2)
+{
+System.out.println(i);
+}
+for(j=2;j<=n2;j+=2)
+{
+System.out.println(j);
+}}
+}
---
+++ Loop.java
@@ -0,0 +1,13 @@
+class Loop
+{
+public static void main(String args[])
+{
+int n=568, s=0 ,b;
+while(n>0)
+b =(n%10);
+s =(s*10)+b;
+n =n/10;{
+System.out.println(b);
+}
+}
+}
